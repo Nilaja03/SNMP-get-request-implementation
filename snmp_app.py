d5234d3d9bf6466d07f0546da380f7c9e9c5c985
Nilanjana Jamindar <88586459+Nilaja03@users.noreply.github.com>
2026-05-03 17:05:28 +0530

    Update snmp_app.py

--- snmp_app.py
+++ snmp_app.py
@@ -31,4 +31,4 @@ def snmp_get():
         return jsonify({"result": result})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
